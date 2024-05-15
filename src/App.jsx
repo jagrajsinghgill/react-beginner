51ec7f3c50cb089385d034e5a8e9e65f6741bc22
Jagraj <jagraj3101@gmail.com>
2024-05-15 15:36:55 +0530

    29 - Class Component Lifecycle Methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react"
-import { Child } from "./Child"
+import { useState } from "react";
+import { Child } from "./Child";
+import { ChildClass } from "./ChildClass";
 
 export default function App() {
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(true);
 
-  const childComponent = show ? <Child /> : null
+  const childComponent = show ? <Child /> : null;
+  const childClassComponent = show ? <ChildClass /> : null;
 
   return (
     <div>
-      <button onClick={() => setShow(currentShow => !currentShow)}>
+      <button onClick={() => setShow((currentShow) => !currentShow)}>
         Show/Hide
       </button>
+      <h1>Function Component</h1>
       {childComponent}
+      <h1>Class Component</h1>
+      {childClassComponent}
     </div>
-  )
+  );
 }
